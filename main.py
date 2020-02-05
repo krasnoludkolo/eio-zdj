e7170402ddccf65f3df2fba8f9021a9287796b77
janek@projmen.pl <janek@projmen.pl>
2020-02-05 17:48:36 +0100

    Drugie

--- main.py
+++ main.py
@@ -1,51 +1,94 @@
+import os
+
 import numpy as np
-from keras.layers import Conv2D, UpSampling2D, InputLayer
+from keras.callbacks import TensorBoard
+from keras.layers import Conv2D, UpSampling2D
+from keras.layers import InputLayer
 from keras.models import Sequential
-from keras.preprocessing.image import img_to_array, load_img
-from skimage.color import rgb2lab, lab2rgb, rgb2gray
+from keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img
+from skimage.color import rgb2lab, lab2rgb
 from skimage.io import imsave
 
-file = 'foto/emilwallner-datasets-colornet-2/images/Train/0bbRiP.jpg'
+# Get images
+X = []
+for filename in os.listdir('Train/'):
+    X.append(img_to_array(load_img('Train/' + filename)))
+X = np.array(X, dtype=float)
 
-image = img_to_array(load_img("woman.jpg"))
-image = np.array(image, dtype=float)
+# Set up train and test data
+split = int(0.95 * len(X))
+Xtrain = X[:split]
+Xtrain = 1.0 / 255 * Xtrain
 
-X = rgb2lab(1.0 / 255 * image)[:, :, 0]
-Y = rgb2lab(1.0 / 255 * image)[:, :, 1:]
-Y /= 128
-X = X.reshape(1, 400, 400, 1)
-Y = Y.reshape(1, 400, 400, 2)
-
-# Building the neural network
 model = Sequential()
-model.add(InputLayer(input_shape=(None, None, 1)))
-model.add(Conv2D(8, (3, 3), activation='relu', padding='same', strides=2))
-model.add(Conv2D(8, (3, 3), activation='relu', padding='same'))
-model.add(Conv2D(16, (3, 3), activation='relu', padding='same'))
-model.add(Conv2D(16, (3, 3), activation='relu', padding='same', strides=2))
-model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
-model.add(Conv2D(32, (3, 3), activation='relu', padding='same', strides=2))
+model.add(InputLayer(input_shape=(256, 256, 1)))
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same', strides=2))
+model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(128, (3, 3), activation='relu', padding='same', strides=2))
+model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(256, (3, 3), activation='relu', padding='same', strides=2))
+model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+model.add(UpSampling2D((2, 2)))
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
 model.add(UpSampling2D((2, 2)))
 model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
-model.add(UpSampling2D((2, 2)))
-model.add(Conv2D(16, (3, 3), activation='relu', padding='same'))
-model.add(UpSampling2D((2, 2)))
 model.add(Conv2D(2, (3, 3), activation='tanh', padding='same'))
-
-# Finish model
+model.add(UpSampling2D((2, 2)))
 model.compile(optimizer='rmsprop', loss='mse')
 
-model.fit(x=X,
-          y=Y,
-          batch_size=1,
-          epochs=1000)
+# Image transformer
+datagen = ImageDataGenerator(
+    shear_range=0.2,
+    zoom_range=0.2,
+    rotation_range=20,
+    horizontal_flip=True)
 
-print(model.evaluate(X, Y, batch_size=1))
-output = model.predict(X)
-output *= 128
+# Generate training data
+batch_size = 10
+
+
+def image_a_b_gen(batch_size):
+    for batch in datagen.flow(Xtrain, batch_size=batch_size):
+        lab_batch = rgb2lab(batch)
+        X_batch = lab_batch[:, :, :, 0]
+        Y_batch = lab_batch[:, :, :, 1:] / 128
+        yield (X_batch.reshape(X_batch.shape + (1,)), Y_batch)
+
+
+# Train model
+tensorboard = TensorBoard(log_dir="output/first_run")
+model.fit_generator(image_a_b_gen(batch_size), callbacks=[tensorboard], epochs=1, steps_per_epoch=1000)
+
+# Save model
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+model.save_weights("model.h5")
+
+# Test images
+Xtest = rgb2lab(1.0 / 255 * X[split:])[:, :, :, 0]
+Xtest = Xtest.reshape(Xtest.shape + (1,))
+Ytest = rgb2lab(1.0 / 255 * X[split:])[:, :, :, 1:]
+Ytest = Ytest / 128
+print(model.evaluate(Xtest, Ytest, batch_size=batch_size))
+
+color_me = []
+for filename in os.listdir('Test/'):
+    color_me.append(img_to_array(load_img('Test/' + filename)))
+color_me = np.array(color_me, dtype=float)
+color_me = rgb2lab(1.0 / 255 * color_me)[:, :, :, 0]
+color_me = color_me.reshape(color_me.shape + (1,))
+
+# Test model
+output = model.predict(color_me)
+output = output * 128
+
 # Output colorizations
-cur = np.zeros((400, 400, 3))
-cur[:, :, 0] = X[0][:, :, 0]
-cur[:, :, 1:] = output[0]
-imsave("img_result.png", lab2rgb(cur))
-imsave("img_gray_version.png", rgb2gray(lab2rgb(cur)))
+for i in range(len(output)):
+    cur = np.zeros((256, 256, 3))
+    cur[:, :, 0] = color_me[i][:, :, 0]
+    cur[:, :, 1:] = output[i]
+    imsave("result/img_" + str(i) + ".png", lab2rgb(cur))
